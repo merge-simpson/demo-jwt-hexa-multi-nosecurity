3db2e983857605ae018297a26e7d45711c802fbf
merge-simpson <letsdev@kakao.com>
2024-01-04 16:35:29 +0900

    feat(common, auth, member profile): ROOT_PACKAGE 상수 관리

--- services/authentication-service/src/main/java/com/example/demo/auth/AuthenticationServiceRunner.java
+++ services/authentication-service/src/main/java/com/example/demo/auth/AuthenticationServiceRunner.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication(scanBasePackages = "com.example")
-@ConfigurationPropertiesScan(basePackages = "com.example")
+import static com.example.common.support.BaseConstants.ROOT_PACKAGE;
+
+@SpringBootApplication(scanBasePackages = ROOT_PACKAGE)
+@ConfigurationPropertiesScan(basePackages = ROOT_PACKAGE)
 public class AuthenticationServiceRunner {
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceRunner.class, args);
