afe2f62e0d28c263b8737cfae04aa937a125d9b3
merge-simpson <letsdev@kakao.com>
2024-01-04 19:15:30 +0900

    feat(auth): jpa entity account

---
+++ services/authentication-service/auth-driven-adapters/auth-rdb-adapter/src/main/java/com/example/auth/rdb/support/AuthRdbConstants.java
@@ -0,0 +1,8 @@
+package com.example.auth.rdb.support;
+
+public final class AuthRdbConstants {
+    public static final String SCHEMA_NAME = "public";
+    public static final String TB_ACCOUNT = "account";
+
+    private AuthRdbConstants() {}
+}
