3411935f82ba190d4d3e50d685d20543f5bc23bd
merge-simpson <letsdev@kakao.com>
2024-01-05 19:33:37 +0900

    refactor(error code): reorder method position

--- common/src/main/java/com/example/common/support/exception/ErrorCode.java
+++ common/src/main/java/com/example/common/support/exception/ErrorCode.java
@@ -6,8 +6,8 @@ public interface ErrorCode { // public enum ClassifiedErrorCode implements Error
     // enum 클래스가 공통으로 갖는 name() 메서드는 에러 코드 관리에 유용하기 때문에 포함.
     // enum 클래스가 공통으로 갖는 ordinal() 메서드는 에러 코드와 무관하기 때문에 제외.
     String name();
-    HttpStatus defaultHttpStatus();
     String defaultMessage();
+    HttpStatus defaultHttpStatus();
     RuntimeException defaultException();
     RuntimeException defaultException(Throwable cause);
 }
